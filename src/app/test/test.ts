06884df691a4c3b2b50ec6ef1a738ee2d71ec35a
vedamanikanta <manikanta.vedam@gmail.com>
2025-09-03 20:00:18 +0530

    created new component and chenged sector differently with class and [ ], and changed the temmplateurl --> template and stylesurl --> styles so that we can work in a single file

---
+++ src/app/test/test.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // selector: '.app-test', for class selector we do like this
+  selector: '[app-test]',
+    // selector: '.app-test',
+  template: `<div>
+  This is 
+  Inline <br/>
+   vedamaniakntaj<br/></div>`,
+  styles: [`div {
+    color: red;
+    font-weight: bold;}`]
+})
+export class Test {
+
+}
